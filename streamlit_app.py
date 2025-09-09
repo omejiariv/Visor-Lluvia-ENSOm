6afe7eba23b03707efb0346bfb9672b6c79268a5
omejiariv <145706652+omejiariv@users.noreply.github.com>
2025-09-08 22:51:01 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,8 @@ import locale
 import base64
 from scipy import stats
 import statsmodels.api as sm
-from pyhomogeneity import pettitt_test, mann_kendall # Importación de la librería para Mann-Kendall
+from pyhomogeneity.mann_kendall import mann_kendall
+from pyhomogeneity.pettitt_test import pettitt_test
 
 # --- Funciones de Carga y Procesamiento ---
 def parse_spanish_dates(date_series):
